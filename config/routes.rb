cdabff2bce3d8d0751e0d3341d741ba9b44e286b
amirsaranBIH <amir.saranbih@gmail.com>
2018-04-16 21:54:46 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  devise_for :users
+ resources :posts do
+ 	resources :comments
+ end
+ root "posts#index"
 
-  resources :posts
-
-  root "posts#index"
-
+ get '/about', to: 'pages#about'
 end
